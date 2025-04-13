67a4dff416366a3482c7edef271cc0cfa8a23264
userquin <userquin@gmail.com>
2025-04-13 16:56:27 +0200

    chore: add `transformAssetUrls` to playgrounds

--- playgrounds/prefix-resolvers/src/App.vue
+++ playgrounds/prefix-resolvers/src/App.vue
@@ -11,29 +11,45 @@ function onClick() {
   <vuetify-app>
     <vuetify-main>
       <vuetify-container class="fill-height">
-        <vuetify-row>
-          <vuetify-col cols="12">
-            <vuetify-card
-              v-vuetify-click-outside="{ handler: onClick }"
-              class="py-4"
-              color="surface-variant"
-              image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-              prepend-icon="mdi-rocket-launch-outline"
-              rounded="lg"
-              variant="tonal"
-            >
-              <template #title>
-                <h2 class="text-h5 font-weight-bold">
-                  Vuetify Auto-Import with Resolvers
-                </h2>
-              </template>
-              <template #actions>
-                Click outside the card: {{ clicks }}
-                <vuetify-btn>Click me</vuetify-btn>
-              </template>
-            </vuetify-card>
-          </vuetify-col>
-        </vuetify-row>
+        <div>
+          <vuetify-img
+            class="mb-4"
+            height="150"
+            src="./assets/logo.png"
+          />
+
+          <div class="mb-8 text-center">
+            <div class="text-body-2 font-weight-light mb-n1">
+              Welcome to
+            </div>
+            <h1 class="text-h2 font-weight-bold">
+              Vuetify
+            </h1>
+          </div>
+          <vuetify-row>
+            <vuetify-col cols="12">
+              <vuetify-card
+                v-vuetify-click-outside="{ handler: onClick }"
+                class="py-4"
+                color="surface-variant"
+                image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
+                prepend-icon="mdi-rocket-launch-outline"
+                rounded="lg"
+                variant="tonal"
+              >
+                <template #title>
+                  <h2 class="text-h5 font-weight-bold">
+                    Vuetify Auto-Import with Resolvers
+                  </h2>
+                </template>
+                <template #actions>
+                  Click outside the card: {{ clicks }}
+                  <vuetify-btn>Click me</vuetify-btn>
+                </template>
+              </vuetify-card>
+            </vuetify-col>
+          </vuetify-row>
+        </div>
       </vuetify-container>
     </vuetify-main>
   </vuetify-app>
