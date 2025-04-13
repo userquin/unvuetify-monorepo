b856b8fa90448a872a12797d35861a72b42f0a77
Andrei Elkin <andreyyolkin@gmail.com>
2025-04-13 17:11:08 +0200

    chore: fix the markup to center the app

--- playgrounds/basic-resolvers/src/App.vue
+++ playgrounds/basic-resolvers/src/App.vue
@@ -10,46 +10,48 @@ function onClick() {
 <template>
   <v-app>
     <v-main>
-      <v-container class="fill-height">
-        <div>
-          <v-img
-            class="mb-4"
-            height="150"
-            src="./assets/logo.png"
-          />
+      <v-container class="fill-height" max-width="900">
+        <v-row justify="center">
+          <v-col>
+            <v-img
+              class="mb-4"
+              height="150"
+              src="./assets/logo.png"
+            />
 
-          <div class="mb-8 text-center">
-            <div class="text-body-2 font-weight-light mb-n1">
-              Welcome to
+            <div class="mb-8 text-center">
+              <div class="text-body-2 font-weight-light mb-n1">
+                Welcome to
+              </div>
+              <h1 class="text-h2 font-weight-bold">
+                Vuetify
+              </h1>
             </div>
-            <h1 class="text-h2 font-weight-bold">
-              Vuetify
-            </h1>
-          </div>
-          <v-row>
-            <v-col cols="12">
-              <v-card
-                v-click-outside="{ handler: onClick }"
-                class="py-4"
-                color="surface-variant"
-                image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-                prepend-icon="mdi-rocket-launch-outline"
-                rounded="lg"
-                variant="tonal"
-              >
-                <template #title>
-                  <h2 class="text-h5 font-weight-bold">
-                    Vuetify Auto-Import with Resolvers
-                  </h2>
-                </template>
-                <template #actions>
-                  Click outside the card: {{ clicks }}
-                  <v-btn>Click me</v-btn>
-                </template>
-              </v-card>
-            </v-col>
-          </v-row>
-        </div>
+            <v-row>
+              <v-col cols="12">
+                <v-card
+                  v-click-outside="{ handler: onClick }"
+                  class="py-4"
+                  color="surface-variant"
+                  image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
+                  prepend-icon="mdi-rocket-launch-outline"
+                  rounded="lg"
+                  variant="tonal"
+                >
+                  <template #title>
+                    <h2 class="text-h5 font-weight-bold">
+                      Vuetify Auto-Import with Resolvers
+                    </h2>
+                  </template>
+                  <template #actions>
+                    Click outside the card: {{ clicks }}
+                    <v-btn>Click me</v-btn>
+                  </template>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
--- playgrounds/prefix-resolvers/src/App.vue
+++ playgrounds/prefix-resolvers/src/App.vue
@@ -10,46 +10,48 @@ function onClick() {
 <template>
   <vuetify-app>
     <vuetify-main>
-      <vuetify-container class="fill-height">
-        <div>
-          <vuetify-img
-            class="mb-4"
-            height="150"
-            src="./assets/logo.png"
-          />
+      <vuetify-container class="fill-height" max-width="900">
+        <vuetify-row justify="center">
+          <vuetify-col>
+            <vuetify-img
+              class="mb-4"
+              height="150"
+              src="./assets/logo.png"
+            />
 
-          <div class="mb-8 text-center">
-            <div class="text-body-2 font-weight-light mb-n1">
-              Welcome to
+            <div class="mb-8 text-center">
+              <div class="text-body-2 font-weight-light mb-n1">
+                Welcome to
+              </div>
+              <h1 class="text-h2 font-weight-bold">
+                Vuetify
+              </h1>
             </div>
-            <h1 class="text-h2 font-weight-bold">
-              Vuetify
-            </h1>
-          </div>
-          <vuetify-row>
-            <vuetify-col cols="12">
-              <vuetify-card
-                v-vuetify-click-outside="{ handler: onClick }"
-                class="py-4"
-                color="surface-variant"
-                image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-                prepend-icon="mdi-rocket-launch-outline"
-                rounded="lg"
-                variant="tonal"
-              >
-                <template #title>
-                  <h2 class="text-h5 font-weight-bold">
-                    Vuetify Auto-Import with Resolvers
-                  </h2>
-                </template>
-                <template #actions>
-                  Click outside the card: {{ clicks }}
-                  <vuetify-btn>Click me</vuetify-btn>
-                </template>
-              </vuetify-card>
-            </vuetify-col>
-          </vuetify-row>
-        </div>
+            <vuetify-row>
+              <vuetify-col cols="12">
+                <vuetify-card
+                  v-vuetify-click-outside="{ handler: onClick }"
+                  class="py-4"
+                  color="surface-variant"
+                  image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
+                  prepend-icon="mdi-rocket-launch-outline"
+                  rounded="lg"
+                  variant="tonal"
+                >
+                  <template #title>
+                    <h2 class="text-h5 font-weight-bold">
+                      Vuetify Auto-Import with Resolvers
+                    </h2>
+                  </template>
+                  <template #actions>
+                    Click outside the card: {{ clicks }}
+                    <vuetify-btn>Click me</vuetify-btn>
+                  </template>
+                </vuetify-card>
+              </vuetify-col>
+            </vuetify-row>
+          </vuetify-col>
+        </vuetify-row>
       </vuetify-container>
     </vuetify-main>
   </vuetify-app>
